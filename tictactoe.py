e649ac9f9056133e5c78aca1d084656d19b5b9ea
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-16 19:35:24 -0700

    "Merging changes"

--- tictactoe.py
+++ tictactoe.py
@@ -9,20 +9,40 @@ class Player:
 
 class Game:
 
-    board = [[None,None,None],[None,None,None],[None,None,None]]
 
-    def move(x,y, player):
+    def __init__(self):
+        board = [["X","O","X"],["O","O","X"],["X","X","X"]]
+        self.board = board
+
+    def move(self,x,y, player):
         pass
 
 
-    def calc_winner():
-        pass 
+    def calc_winner(self):
+        for space in range(3):
+            # Loops through columns to check for vertical win.
+            if self.board[space] == 'X' and self.board[space+3] == 'X' and self.board[space+6] == 'X':
+                return win_condition == True
 
-    def is_full():
-        pass
+            # Loops though rows to check for horizontal win
+            elif self.board[(space*3)] == 'X' and self.board[(space*3)+1] == 'X' and self.board[(space*3)+2] == 'X':
+                return win_condition == True
 
-    def is_game_over():
-        pass
+            # Loops through and checks for either possible diagonal win
+            elif self.board[0] == 'X' and self.board[4] == 'X' and self.board[8] == 'X':
+                return win_condition == True
+
+            elif self.board[2] == 'X' and self.board[4] == 'X' and self.board[6] == 'X':
+                return 
+
+    def is_full(self):
+        for spot in self.board:
+            print(spot)
+
+    def is_game_over(self):
+        
+
+
 
 
 
@@ -36,3 +56,6 @@ p1 = Player("Dan", "X")
 print(p1.name + p1.token)
 
 
+g1 = Game()
+g1.is_full()
+g1.is_game_over()
