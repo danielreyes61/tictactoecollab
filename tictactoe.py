53e7f0e61bcdaacdeee7cd4be7874f946554bbaa
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-16 20:26:00 -0700

    is full merge

--- tictactoe.py
+++ tictactoe.py
@@ -6,47 +6,49 @@ class Player:
 
 class Game:
     def __init__(self):
-        self.board = [["X","O","X"],["O","O","X"],["X","X","X"]]
+        self.board = [["X",None,"X"],["O","O","X"],["X","X","X"]]
         self.fullBoard = False
+        self.x = -1
+        self.y = -1
+        self.player1 = None
+        self.player2 = None
+        self.win_condition = None
+        self.is_full_condition = None
 
-    def move(x,y, player):
+    def move(self):
         pass
 
-    def calc_winner(self, board):
+    def calc_winner(self):
         for space in range(3):
             # Loops through columns to check for vertical win.
             if self.board[space] == 'X' and self.board[space+3] == 'X' and self.board[space+6] == 'X':
-                return win_condition == True
+                self.win_condition == True
 
             # Loops though rows to check for horizontal win
             elif self.board[(space*3)] == 'X' and self.board[(space*3)+1] == 'X' and self.board[(space*3)+2] == 'X':
-                return win_condition == True
+                self.win_condition == True
 
             # Loops through and checks for either possible diagonal win
             elif self.board[0] == 'X' and self.board[4] == 'X' and self.board[8] == 'X':
-                return win_condition == True
+                self.win_condition == True
 
             elif self.board[2] == 'X' and self.board[4] == 'X' and self.board[6] == 'X':
-                return win_condition == True
+                self.win_condition == True
 
-    def is_full(self, board):
+    def is_full(self):
         for spot in self.board:
-            print(spot)
             if spot != None:
-                return fullBoard == True
+                self.is_full_condition = True
             else:
-                continue
+                self.is_full_condition = False
+                #return self.fullBoard == True
+            #else:
+                #continue
 
-<<<<<<< HEAD
-
-    def is_game_over(self):
-        if self.fullBoard == True:
-=======
     def is_game_over(self, calc_winner, is_full):
-        if fullBoard == True:
->>>>>>> a9cbc3b7a16e59a6951bfcf54dd1820786ed1ee9
+        if self.fullBoard == True:
             outcome = 'No more spaces. GG.'
-        elif win_condition == True:
+        elif self.win_condition == True:
             outcome = f''
 
             
@@ -54,14 +56,13 @@ def __repr__(self,board):
     print(f"{board[0]}\n{board[1]}\n{board[2]}")
     pass
 
-g1 = Game()
-#g1.is_full(board)
-
 p1 = Player("Dan", "X")
-<<<<<<< HEAD
-print(p1.name + p1.token)
+p2 = Player("Ted", "O")
+g1 = Game()
+g1.player1 = p1
+g1.player2 = p2
 
-print(g1.is_game_over())
-=======
 print(p1.name + p1.token)
->>>>>>> a9cbc3b7a16e59a6951bfcf54dd1820786ed1ee9
+g1.is_full()
+print(g1.is_full_condition)
+
